6b29f88f4a41684f0040c873ba082c60e53761c3
zanishbilal1212 <zanishbilal72@gmail.com>
2025-07-04 18:31:49 +0500

    Moved gmail_app_password to environment variable

--- app/gui_app.py
+++ app/gui_app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, render_template
 from app.agents.orchestrator_agent import run_orchestrator
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="app/templates")
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
